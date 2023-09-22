0d98f30eeeb0f62876af289e4ea7af916f856690
Ítalo Rizzon <italomarquesrizzon@gmail.com>
2023-09-22 19:56:37 -0300

    AdSense teste

--- src/components/AdSense/index.js
+++ src/components/AdSense/index.js
@@ -1,35 +1,32 @@
-import React, { useEffect  } from 'react';
+import React, { useEffect } from 'react';
 
-const AdsComponent = (props) => {
-    const { dataAdSlot } = props;  
+const AdsComponent = ({ dataAdSlot }) => {
+  useEffect(() => {
+    const loadAds = () => {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+    };
 
+    if (window.adsbygoogle) {
+      loadAds();
+    } else {
+      const script = document.createElement('script');
+      script.async = true;
+      script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
+      script.onload = loadAds;
+      document.head.appendChild(script);
+    }
+  }, []);
 
-
-    useEffect(() => {
-
-        try {
-            (window.adsbygoogle = window.adsbygoogle || []).push({});
-        }
-
-        catch (e) {
-
-        }
-
-    },[]);
-
-
-
-    return (
-        <>
-            <ins class="adsbygoogle"
-                style="display:block"
-                data-ad-client="ca-pub-3542447345082980"
-                data-ad-slot={dataAdSlot}
-                data-ad-format="auto"
-                data-full-width-responsive="true">
-            </ins>
-        </>
-    );
+  return (
+    <ins
+      className="adsbygoogle"
+      style={{ display: 'block' }}
+      data-ad-client="ca-pub-3542447345082980"
+      data-ad-slot={dataAdSlot}
+      data-ad-format="auto"
+      data-full-width-responsive="true"
+    ></ins>
+  );
 };
 
 export default AdsComponent;
